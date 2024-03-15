67a2b18d5ee4ea530479b8b35733fb63417a5de7
ducdosomething <duclehoang211201@gmail.com>
2024-03-15 10:58:20 +0700

    upload th2

---
+++ th2/TestMyLinkedList.java
@@ -0,0 +1,16 @@
+package th2;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MylinkedList ll = new MylinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4, 9);
+        ll.add(4, 9);
+
+        ll.printList();
+    }
+}
